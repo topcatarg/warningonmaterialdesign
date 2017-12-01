9bde6b532d2bc16c10f6f8d693fdf6d97538df65
gbianchi <gbianchi@afip.gob.ar>
2017-12-01 12:08:17 -0300

    Agregar archivos de proyecto.

---
+++ TestWarning/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWarning
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Progreso.Visibility = Visibility.Visible;
+            await Task.Delay(5000);
+            Progreso.Visibility = Visibility.Hidden;
+        }
+    }
+}
